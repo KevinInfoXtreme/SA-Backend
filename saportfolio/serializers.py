bdc7b391a88522ddf6304c7f1db4f6e2fb47513b
Kevin <kevin@info-xtreme.com>
2025-05-28 17:00:06 -0400

    Add requirements.txt for Render.14

--- saportfolio/serializers.py
+++ saportfolio/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
+    def get_image(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.image.url)
+
     class Meta:
         model = Project
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'image']
