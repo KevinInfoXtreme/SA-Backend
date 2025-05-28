b2f500172ddb98b191f559c47dcd06342ce97bf3
Kevin <kevin@info-xtreme.com>
2025-05-28 16:58:46 -0400

    Add requirements.txt for Render.13

--- saportfolio/models.py
+++ saportfolio/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image1 = models.ImageField(upload_to='project_images/', blank=True, null=True)
-    image2 = models.ImageField(upload_to='project_images/', blank=True, null=True)
+    image = models.ImageField(upload_to='project_images/')  # 👈 Important
 
     def __str__(self):
         return self.title
