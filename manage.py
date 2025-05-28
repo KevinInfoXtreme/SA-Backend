aef7d6b42ad7ae65fd90f5d25897ac9c13c6f6e7
Kevin <kevin@info-xtreme.com>
2025-05-28 16:10:26 -0400

    Add requirements.txt for Render.6

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PortfolioManager.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SAManager.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
