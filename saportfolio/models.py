a76c2c9bcb2a96e34ab6db7eaa7f4f67d1c095e8
Kevin <kevin@info-xtreme.com>
2025-05-29 12:07:22 -0400

    Add image upload2

--- saportfolio/models.py
+++ saportfolio/models.py
@@ -5,7 +5,6 @@ from cloudinary.models import CloudinaryField  # optional, or use ImageField
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField()  # No upload_to needed
 
     def __str__(self):
         return self.title
